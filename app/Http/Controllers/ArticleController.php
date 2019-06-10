e815a7f892fc2338b9052ebfb5e5e3226fe67be0
xD <anonnagibon@gmail.com>
2019-06-11 00:01:32 +0300

    articles on main page

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all();
+        return view('welcome', compact('articles'));
+    }
+
+    public function show(Request $request, $id)
+    {
+
+    }
+}
